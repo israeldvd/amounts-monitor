6a1af50b8022daa70674eb7aa095059d6d102801
Israel David <israelcaetano21@hotmail.com>
2022-08-18 16:49:19 -0300

    Update components App & AmountItem
    - Move date into AmountItem component
    - Exchange raw-value with dynamic-input values (simple usage)
      - use object for an item

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div>
       <header>
         <h1>Amounting the savings</h1>
-        <div>2022-08-16</div>
       </header>
       <AmountItem></AmountItem>
     </div>
--- react-complete/src/components/AmountItem.js
+++ react-complete/src/components/AmountItem.js
@@ -1,11 +1,20 @@
 import "./AmountItem.css";
 
 function AmountItem(params) {
+  const item = {
+    date: new Date(2022, 8, 18),
+    title: "Streaming and TV",
+    cost: 49.99,
+  };
+
   return (
     <div>
       <div className="whole_amount_item">
-        <h2 className="item_description">Streaming and TV</h2>
-        <div className="item_cost">R$ 49,99</div>
+        <div className="item_info">
+          <h2 className="item_description">{item.title}</h2>
+          <span>{item.date.toISOString()}</span>
+        </div>
+        <div className="item_cost">R$ {item.cost}</div>
       </div>
     </div>
   );
