cd06a725349fc44fa4dc74464e33a8b4461a0037
Israel David <israelcaetano21@hotmail.com>
2022-08-19 19:24:01 -0300

    Edit App.js: mapping and formatting
    - use mapping to insert multiple dynamic components
    - avoid writing multie lines with AmountItem 'element'
    - format App.js with starndardized spaces number

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -1,44 +1,50 @@
 import AmountItem from "./components/AmountItem";
 
 function App() {
-  const items = [
-    {
-      id: "0",
-      title: "Car and gas",
-      cost: 50.0,
-      date: new Date(2022, 8 - 1, 17),
-    },
-    {
-      id: "1",
-      title: "Education bills",
-      cost: 400.0,
-      date: new Date(2022, 8 - 1, 16),
-    },
-    {
-      id: "2",
-      title: "Other",
-      cost: 55.0,
-      date: new Date(2022, 8 - 1, 18),
-    },
-    {
-      id: "3",
-      date: new Date(2022, 8 - 1, 18),
-      title: "Streaming and TV",
-      cost: 49.99,
-    },
-  ];
+    const items = [
+        {
+            id: "0",
+            title: "Car and gas",
+            cost: 50.0,
+            date: new Date(2022, 8 - 1, 17),
+        },
+        {
+            id: "1",
+            title: "Education bills",
+            cost: 400.0,
+            date: new Date(2022, 8 - 1, 16),
+        },
+        {
+            id: "2",
+            title: "Other",
+            cost: 55.0,
+            date: new Date(2022, 8 - 1, 18),
+        },
+        {
+            id: "3",
+            date: new Date(2022, 8 - 1, 18),
+            title: "Streaming and TV",
+            cost: 49.99,
+        },
+    ];
 
-  return (
-    <div>
-      <header>
-        <h1>Amounting the savings</h1>
-      </header>
-      <AmountItem item={items[0]}></AmountItem>
-      <AmountItem item={items[1]}></AmountItem>
-      <AmountItem item={items[2]}></AmountItem>
-      <AmountItem item={items[3]}></AmountItem>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <h1>Amounting the savings</h1>
+            </header>
+            {items.map((id, title, cost, date) => {
+                return (
+                    <AmountItem
+                        id={id}
+                        title={title}
+                        cost={cost}
+                        date={date}
+                    ></AmountItem>
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
