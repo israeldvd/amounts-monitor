2b872f0c9d0e6a23a4e6c15c73c43405e512f007
Israel David <israelcaetano21@hotmail.com>
2022-08-29 18:52:25 -0300

    Use Card component by means of composition
    - Use a no-self-closing component which saves code
    - Its main purpose being defining a card-like "box"
    - Import it where it is due
    - Define its class (then, styles)

--- react-complete/src/components/AmountItem.js
+++ react-complete/src/components/AmountItem.js
@@ -1,9 +1,16 @@
+import Card from "./Card";
 import ItemDate from "./ItemDate";
 import "./AmountItem.css";
 
 function AmountItem(props) {
     return (
+        <Card className="whole_amount_item">
+            <div className="item_info">
+                <h2 className="item_description">{props.title}</h2>
+                <ItemDate date={props.date} />
             </div>
+            <div className="item_cost">R$ {props.cost}</div>
+        </Card>
     );
 }
 
