d390cd5436c51e0e714cefdb7c57495dca2137b6
Israel David <israelcaetano21@hotmail.com>
2022-08-29 20:06:45 -0300

    Sort amount items by date

--- react-complete/src/components/Items.js
+++ react-complete/src/components/Items.js
@@ -5,17 +5,19 @@ import "./Items.css";
 function Items(props) {
     return (
         <Card className="items">
-            {props.items.map((item) => {
-                return (
-                    <AmountItem
-                        key={item.id}
-                        id={item.id}
-                        title={item.title}
-                        cost={item.cost}
-                        date={item.date}
-                    />
-                );
-            })}
+            {props.items
+                .sort((a, b) => b.date - a.date)
+                .map((item) => {
+                    return (
+                        <AmountItem
+                            key={item.id}
+                            id={item.id}
+                            title={item.title}
+                            cost={item.cost}
+                            date={item.date}
+                        />
+                    );
+                })}
         </Card>
     );
 }
