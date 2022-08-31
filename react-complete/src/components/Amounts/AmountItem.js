30495278393ba0cd897c5542613cdc80fa2ed0cd
Israel David <israelcaetano21@hotmail.com>
2022-08-31 12:49:01 -0300

    Add experimental useState functionality

--- react-complete/src/components/Amounts/AmountItem.js
+++ react-complete/src/components/Amounts/AmountItem.js
@@ -1,17 +1,30 @@
+import { useState } from "react";
+
 import Card from "../UI/Card";
 import ItemDate from "./ItemDate";
 import "./AmountItem.css";
 
 const AmountItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const titleChangeHandler = () => {
+        setTitle("Updated!");
+    };
+
     return (
         <Card className="whole_amount_item">
-            <div className="item_info">
-                <h2 className="item_description">{props.title}</h2>
-                <ItemDate date={props.date} />
+            <div className="item_data">
+                <div className="item_info">
+                    <h2 className="item_description">{title}</h2>
+                    <ItemDate date={props.date} />
+                </div>
+                <div className="item_cost">R$ {props.cost}</div>
             </div>
-            <div className="item_cost">R$ {props.cost}</div>
+            <div>
+                <button onClick={titleChangeHandler}>Change title</button>
+            </div>
         </Card>
     );
-}
+};
 
 export default AmountItem;
