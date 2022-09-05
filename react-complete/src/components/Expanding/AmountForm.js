87f8ae790dd9ba44b6789a6501c9230c6869ce03
Israel David <israelcaetano21@hotmail.com>
2022-09-05 12:41:55 -0300

    Prototype user input handler
    - work with multiple states

--- react-complete/src/components/Expanding/AmountForm.js
+++ react-complete/src/components/Expanding/AmountForm.js
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import "./AmountForm.css";
 
 const AmountForm = () => {
+    const [enteredDescription, setEnteredDescription] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+
+    const descripChangeHandler = (event) => {
+        setEnteredDescription(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    };
+
     return (
         <form action="POST">
             <div className="settings">
                 <div className="setter">
                     <label htmlFor="set-description">Description</label>
-                    <input type="text" name="set-description" />
+                    <input
+                        type="text"
+                        name="set-description"
+                        onChange={descripChangeHandler}
+                    />
                 </div>
                 <div className="setter">
                     <label htmlFor="set-amount">Amount</label>
@@ -16,6 +37,7 @@ const AmountForm = () => {
                         name="set-amount"
                         min="0.01"
                         step="0.01"
+                        onChange={amountChangeHandler}
                     />
                 </div>
                 <div className="setter">
@@ -25,6 +47,7 @@ const AmountForm = () => {
                         name="set-date"
                         min="2000-01-01"
                         max="2050-12-31"
+                        onChange={dateChangeHandler}
                     />
                 </div>
             </div>
