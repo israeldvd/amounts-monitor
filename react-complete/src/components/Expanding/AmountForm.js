a9eb62196d9b9d7b18af491cc3a711d074360f01
Israel David <israelcaetano21@hotmail.com>
2022-09-20 11:19:36 -0300

    Include form-validation script and style
    - Set required to all fields and validation when it is not enough
    - Validation-coloring according to empty field
    - As the usage of name spread over the code
        - implemenent them by means of variables, instead
        - set a constant object to get hold of them

--- react-complete/src/components/Expanding/AmountForm.js
+++ react-complete/src/components/Expanding/AmountForm.js
@@ -2,29 +2,63 @@ import { useState } from "react";
 import "./AmountForm.css";
 
 const AmountForm = (props) => {
+    const inputIdentifiers = {
+        description: "item-description-input",
+        date: "item-date-input",
+        cost: "item-cost-input",
+    };
+
     const [enteredInput, setInput] = useState({
-        "item-description-input": "",
-        "item-cost-input": 0,
-        "item-date-input": "",
+        [inputIdentifiers.description]: "",
+        [inputIdentifiers.cost]: 0,
+        [inputIdentifiers.date]: "",
+    });
+
+    const [isValid, setValidationState] = useState({
+        [inputIdentifiers.description]: true,
     });
 
     const inputChangeHandler = (event) => {
-        if (event.target.name in enteredInput)
+        const targetName = event.target.name;
+        if (targetName in enteredInput) {
             setInput((prevState) => {
                 return {
                     ...prevState,
-                    [event.target.name]: event.target.value,
+                    [targetName]: event.target.value,
                 };
             });
+        }
+
+        if (targetName in isValid) {
+            setValidationState((previousValidationState) => {
+                return { ...previousValidationState, [targetName]: true };
+            });
+        }
     };
 
     const submitHandler = (event) => {
         event.preventDefault();
 
+        const emptyInputFields = Array.from(
+            Array.from(event.target.querySelectorAll(".setter input")).filter(
+                (input) => input.value.trim().length === 0
+            )
+        );
+
+        if (emptyInputFields.length) {
+            setValidationState({ [inputIdentifiers.description]: false });
+
+            emptyInputFields.forEach((element) => {
+                element.placeholder = "Insert a valid text here";
+            });
+
+            return;
+        }
+
         const amountsData = {
-            description: enteredInput["item-description-input"],
-            cost: parseFloat(enteredInput["item-cost-input"]),
-            date: new Date(enteredInput["item-date-input"] + "T00:00:00"),
+            description: enteredInput[inputIdentifiers.description],
+            cost: parseFloat(enteredInput[inputIdentifiers.cost]),
+            date: new Date(enteredInput[inputIdentifiers.date] + "T00:00:00"),
         };
 
         setInput(
@@ -40,42 +74,58 @@ const AmountForm = (props) => {
         <form action="POST" onSubmit={submitHandler}>
             <div className="settings">
                 <div className="setter">
-                    <label htmlFor="item-description-input">Description</label>
+                    <label htmlFor={inputIdentifiers.description}>
+                        Description
+                    </label>
                     <input
                         type="text"
-                        value={enteredInput["item-description-input"]}
-                        id="item-description-input"
-                        name="item-description-input"
+                        value={
+                            isValid[inputIdentifiers.description]
+                                ? enteredInput[inputIdentifiers.description]
+                                : ""
+                        }
+                        id={inputIdentifiers.description}
+                        name={inputIdentifiers.description}
+                        className={
+                            isValid[inputIdentifiers.description]
+                                ? ""
+                                : "invalid"
+                        }
                         onChange={inputChangeHandler}
+                        required
                     />
                 </div>
                 <div className="setter">
-                    <label htmlFor="item-cost-input">Cost</label>
+                    <label htmlFor={inputIdentifiers.cost}>Cost</label>
                     <input
-                        id="item-cost-input"
-                        name="item-cost-input"
+                        id={inputIdentifiers.cost}
+                        name={inputIdentifiers.cost}
                         type="number"
-                        value={enteredInput["item-cost-input"]}
+                        value={enteredInput[inputIdentifiers.cost]}
                         min="0.01"
                         step="0.01"
                         onChange={inputChangeHandler}
+                        required
                     />
                 </div>
                 <div className="setter">
-                    <label htmlFor="item-date-input">Date</label>
+                    <label htmlFor={inputIdentifiers.date}>Date</label>
                     <input
                         type="date"
-                        value={enteredInput["item-date-input"]}
-                        id="item-date-input"
-                        name="item-date-input"
+                        value={enteredInput[inputIdentifiers.date]}
+                        id={inputIdentifiers.date}
+                        name={inputIdentifiers.date}
                         min="2000-01-01"
                         max="2050-12-31"
                         onChange={inputChangeHandler}
+                        required
                     />
                 </div>
             </div>
             <div className="action">
-                <button type='button' onClick={props.onUserCancel}>Cancel</button>
+                <button type="button" onClick={props.onUserCancel}>
+                    Cancel
+                </button>
                 <button type="submit">Add new</button>
             </div>
         </form>
