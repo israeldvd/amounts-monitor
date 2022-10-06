64dc72487a8c7bd511825d3be053b830317c1bbb
Israel David <israelcaetano21@hotmail.com>
2022-10-06 17:58:27 -0300

    Refactor: implement Reducer for form state
    - Move constants outside components
    - Comment out (to-be deleted) alternatives to
        - state-changer (inside dispatchForm)
        - validity status updater (inside inputChangeHandler)
    - Add useReducer to store value and validity
    - Simplify invalid-inputs-finder function
    - Update state (classNames) according to new state management
    - Create input validity handler function and link onBlur to it

--- amounts-monitor/src/components/Expanding/AmountForm.js
+++ amounts-monitor/src/components/Expanding/AmountForm.js
@@ -1,24 +1,95 @@
-import { useRef, useState, Fragment } from "react";
+import { useRef, useState, Fragment, useReducer } from "react";
 
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 
 import styles from "./AmountForm.module.css";
 
-const AmountForm = (props) => {
-    const descriptionInputName = "item-description-input";
-    const dateInputName = "item-date-input";
-    const costInputName = "item-cost-input";
+const descriptionInputName = "item-description-input";
+const dateInputName = "item-date-input";
+const costInputName = "item-cost-input";
 
+const dispatchForm = (state, action) => {
+    // name-specific value state-changer
+    // if (action.type === "USER_INPUT" && action.name === descriptionInputName)
+    //     return {
+    //         description: {
+    //             value: action.val,
+    //             isValid: state.description.isValid, //repeat
+    //         },
+    //         cost: state.cost, //repeat state
+    //         date: state.date, //repeat state
+    //         isValid:
+    //             action.val.trim().length > 0 &&
+    //             state.cost.isValid &&
+    //             state.date.isValid,
+    //     };
+
+    // generic input-value state update
+    if (action.type === "USER_INPUT") {
+        return {
+            ...state,
+            [action.name]: {
+                ...state[action.name],
+                value: action.val,
+            },
+            isValid: state.isValid,
+        };
+    }
+
+    // after onBlur, validate the input state (validation method may vary, accoring to name)
+    if (action.type === "INPUT_BLUR") {
+        const inputValidation = state[action.name].value.trim().length > 0;
+        return {
+            ...state,
+            [action.name]: {
+                ...state[action.name],
+                isValid: inputValidation,
+            },
+            isValid: state.isValid && inputValidation,
+        };
+    }
+
+    // otherwise, this would 'reset' all entries
+    return {
+        [descriptionInputName]: {
+            value: "",
+            isValid: false,
+        },
+        [costInputName]: {
+            value: "",
+            isValid: false,
+        },
+        [dateInputName]: {
+            value: "",
+            isValid: false,
+        },
+        isValid: false,
+    };
+};
+
+const AmountForm = (props) => {
     const descriptionInputRef = useRef();
     const dateInputRef = useRef();
     const costInputRef = useRef();
 
-    const [isValid, setValidationState] = useState({
-        [descriptionInputName]: true,
-    });
-
     const [error, setError] = useState();
+
+    const [formState, setFormState] = useReducer(dispatchForm, {
+        [descriptionInputName]: {
+            value: "",
+            isValid: null,
+        },
+        [costInputName]: {
+            value: "",
+            isValid: null,
+        },
+        [dateInputName]: {
+            value: "",
+            isValid: null,
+        },
+        isValid: null,
+    });
 
     const clearInputs = (...inputs) => {
         inputs.forEach((currentInput) => (currentInput.value = ""));
@@ -28,20 +99,16 @@ const AmountForm = (props) => {
         formInputs,
         placeholder = "Insert a valid text here"
     ) => {
-        const emptyInputFields = formInputs.filter(
-            (input) => input.value.trim().length === 0
-        );
-
         let inputNames = [];
-        if (emptyInputFields.length) {
-            setValidationState({ [descriptionInputName]: false });
 
-            emptyInputFields.forEach((element) => {
+        formInputs.forEach((element) => {
+            if (element.value.trim().length === 0) {
+                setFormState({ type: "INPUT_BLUR", name: element.name });
                 inputNames.push(element.name);
                 element.value = "";
                 element.placeholder = placeholder;
-            });
-        }
+            }
+        });
 
         return inputNames;
     };
@@ -49,11 +116,21 @@ const AmountForm = (props) => {
     const inputChangeHandler = (event) => {
         const targetName = event.target.name;
 
-        if (isValid[targetName] !== undefined) {
-            setValidationState((previousValidationState) => {
-                return { ...previousValidationState, [targetName]: true };
-            });
-        }
+        // if (isValid[targetName] !== undefined) {
+        //     setValidationState((previousValidationState) => {
+        //         return { ...previousValidationState, [targetName]: true };
+        //     });
+        // }
+
+        setFormState({
+            type: "USER_INPUT",
+            name: targetName,
+            val: event.target.value,
+        });
+    };
+
+    const validateInputHandler = (event) => {
+        setFormState({ type: "INPUT_BLUR", name: event.target.name });
     };
 
     const submitHandler = (event) => {
@@ -115,42 +192,56 @@ const AmountForm = (props) => {
                             Description
                         </label>
                         <input
+                            name={descriptionInputName}
                             type="text"
                             id={descriptionInputName}
-                            name={descriptionInputName}
                             ref={descriptionInputRef}
                             className={
-                                isValid[descriptionInputName]
-                                    ? ""
-                                    : styles["invalid"]
+                                formState[descriptionInputName].isValid ===
+                                false
+                                    ? styles["invalid"]
+                                    : ""
                             }
                             onChange={inputChangeHandler}
+                            onBlur={validateInputHandler}
                             required
                         />
                     </div>
                     <div className={styles["setter"]}>
                         <label htmlFor={costInputName}>Cost</label>
                         <input
-                            id={costInputName}
                             name={costInputName}
+                            id={costInputName}
                             ref={costInputRef}
+                            className={
+                                formState[costInputName].isValid === false
+                                    ? styles["invalid"]
+                                    : ""
+                            }
                             type="number"
                             min="0.01"
                             step="0.01"
                             onChange={inputChangeHandler}
+                            onBlur={validateInputHandler}
                             required
                         />
                     </div>
                     <div className={styles["setter"]}>
                         <label htmlFor={dateInputName}>Date</label>
                         <input
+                            name={dateInputName}
                             type="date"
                             id={dateInputName}
-                            name={dateInputName}
                             ref={dateInputRef}
+                            className={
+                                formState[dateInputName].isValid === false
+                                    ? styles["invalid"]
+                                    : ""
+                            }
                             min="2000-01-01"
                             max="2050-12-31"
                             onChange={inputChangeHandler}
+                            onBlur={validateInputHandler}
                             required
                         />
                     </div>
@@ -159,7 +250,9 @@ const AmountForm = (props) => {
                     <Button type="button" onClick={props.onUserCancel}>
                         Cancel
                     </Button>
-                    <Button type="submit">Add new</Button>
+                    <Button type="submit" disabled={!formState.isValid}>
+                        Add new
+                    </Button>
                 </div>
             </form>
         </Fragment>
--- amounts-monitor/src/components/UI/Button.js
+++ amounts-monitor/src/components/UI/Button.js
@@ -3,7 +3,7 @@ import classes from "./Button.module.css";
 const Button = (props) => {
     return (
         <button
-            className={classes.button}
+            className={`${classes.button}`}
             type={props.type || "button"}
             onClick={props.onClick}
         >
